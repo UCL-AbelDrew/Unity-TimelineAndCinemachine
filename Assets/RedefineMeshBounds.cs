0e0268378a2a5d0fa9cb78e31dd4023a51f51bea
AbelDrew <43228983+AbelDrew@users.noreply.github.com>
2021-02-18 10:35:51 +0000

    Initial commit

---
+++ Assets/RedefineMeshBounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RedefineMeshBounds : MonoBehaviour
+{
+    public bool m_redefine;
+    public SkinnedMeshRenderer m_skinnedMeshRenderer;
+ 
+   
+    void Update()
+    {
+        if (m_redefine)
+        {
+            Redefine();
+            m_redefine = false;
+        }
+        
+    }
+
+    private void Redefine()
+    {
+        Mesh mesh = m_skinnedMeshRenderer.sharedMesh;
+        mesh.RecalculateBounds();
+    }
+
+}
---
+++ Assets/Scripts/DirectorAnimationCurve.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class DirectorAnimationCurve : MonoBehaviour
+{
+    // Animate this moving from 0-1 in the TimeLine;
+    public float m_curvePosition;
+    public AnimationCurve m_animationCurve;
+    public PlayableDirector m_playableDirector;
+
+    public bool m_playOnAwake;
+
+    private PlayableGraph m_playableGraph;
+    private bool m_evaluateCurve;
+    private float m_frameTime;
+
+    private void Start()
+    {
+        m_playableGraph = m_playableDirector.playableGraph;
+        if (m_playOnAwake)
+        {
+            BeginCurve();
+        }
+
+    }
+
+    public void BeginCurve()
+    {
+        m_evaluateCurve = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_evaluateCurve)
+        {
+            m_frameTime = m_animationCurve.Evaluate(m_curvePosition);
+            m_frameTime *= Time.deltaTime;
+
+            try
+            {
+                m_playableGraph.Evaluate(m_frameTime);
+            }
+            catch
+            {
+                Debug.Log("Playable Graph not found, it may have been destroyed: ");
+                m_evaluateCurve = false;
+            }
+        }
+    }
+}
